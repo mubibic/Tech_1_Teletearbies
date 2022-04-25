6666c8e4dba60ef888285c64093f4e9a84cff042
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-04-25 16:02:25 +0200

    project

---
+++ src/main/java/com/example/tech_1_teletearbies/Tech1TeletearbiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tech_1_teletearbies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech1TeletearbiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tech1TeletearbiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tech_1_teletearbies/Tech1TeletearbiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tech_1_teletearbies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech1TeletearbiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
